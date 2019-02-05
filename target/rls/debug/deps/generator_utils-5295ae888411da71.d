9f0df11038f2d44f986d51bccec77bd01ebdb36a
Dodo <kasper199914@gmail.com>
2019-02-05 16:55:34 +0100

    licence

--- target/rls/debug/deps/generator_utils-5295ae888411da71.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Kasper\Documents\Rust\githubs\generator-utils\target\rls\debug\deps\generator_utils-5295ae888411da71.rmeta: src\lib.rs
-
-c:\Users\Kasper\Documents\Rust\githubs\generator-utils\target\rls\debug\deps\generator_utils-5295ae888411da71.d: src\lib.rs
-
-src\lib.rs:
--- target/rls/debug/deps/generator_utils-fe5c881f8f01bec9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Kasper\Documents\Rust\githubs\generator-utils\target\rls\debug\deps\generator_utils-fe5c881f8f01bec9.rmeta: src\lib.rs
-
-c:\Users\Kasper\Documents\Rust\githubs\generator-utils\target\rls\debug\deps\generator_utils-fe5c881f8f01bec9.d: src\lib.rs
-
-src\lib.rs:
