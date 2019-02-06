fe85556176128087ddb0dca92a1c1eda9e0d0a8c
Dodo <kasper199914@gmail.com>
2019-02-06 16:07:38 +0100

    neat, Iterator is implementable on G: Unpin!

--- src/iter.rs
+++ src/iter.rs
@@ -1,6 +1,7 @@
 use std::{
     ops::{Generator, GeneratorState},
     pin::Pin,
+    iter::IntoIterator,
 };
 
 pub struct GenIter<G>(Option<G>);
@@ -18,7 +19,7 @@ impl<'a, G: Generator> Iterator for Pin<&mut GenIter<G>> {
 
         let _self: Pin<&mut GenIter<G>> = self.as_mut();
 
-        let mut gen: Pin<&mut Option<G>> = unsafe { _self.map_unchecked_mut(|geniter| &mut geniter.0) };
+        let gen: Pin<&mut Option<G>> = unsafe { _self.map_unchecked_mut(|geniter| &mut geniter.0) };
 
         let gen: Option<Pin<&mut G>> = Option::as_pin_mut(gen);
 
@@ -31,4 +32,30 @@ impl<'a, G: Generator> Iterator for Pin<&mut GenIter<G>> {
             None => None,
         }
     }
+}
+
+impl <G> Iterator for GenIter<G> where G: Generator + Unpin {
+    type Item = G::Yield;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Pin::new(self).next()
+    }
+}
+
+#[test]
+fn test_iter() {
+    let mut g = || {
+        for i in 0..5u32 {
+            yield i
+        }
+    };
+
+    let mut iter = GenIter::new(g);
+
+    for (n, i) in (0..5).zip(iter.by_ref()) {
+        assert!(n == i);
+    }
+
+    // we've gone trough all items of the generator, and we don't panick if we still call .next()
+    assert!(iter.next().is_none());
 }
